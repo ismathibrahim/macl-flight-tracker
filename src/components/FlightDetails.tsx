1ad6e365b1ae02aead20054b81f9d72bc4caacac
Ismath Ibrahim <ismathibrahim0@gmail.com>
2025-02-16 04:04:55 +0500

    update

--- src/components/FlightDetails.tsx
+++ src/components/FlightDetails.tsx
@@ -2,7 +2,13 @@ import { Drawer } from "vaul";
 import { Flight } from "../lib/types";
 import StatusBadge, { UIStatus } from "./StatusBadge";
 import { LogoMap, statusMap } from "../lib/utils";
-import { Airplane, AirplaneLanding, AirplaneTakeoff } from "@phosphor-icons/react";
+import {
+  Airplane,
+  AirplaneLanding,
+  AirplaneTakeoff,
+} from "@phosphor-icons/react";
+import { Dialog } from "radix-ui";
+import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
 
 interface Props {
   flight: Flight;
@@ -15,7 +21,21 @@ const FlightDetails = ({ flight, isOpen, onOpenChange }: Props) => {
     <Drawer.Root open={isOpen} onOpenChange={onOpenChange}>
       <Drawer.Portal>
         <Drawer.Overlay className="fixed inset-0 bg-black/40" />
-        <Drawer.Content className="bg-bg-base h-fit fixed bottom-0 left-0 right-0 outline-none rounded-t-6xl">
+        <Drawer.Content
+          title="Flight Details"
+          aria-label="Flight Details"
+          aria-describedby="Flight Details"
+          aria-description={flight.flightNumber}
+          className="bg-bg-base h-fit fixed bottom-0 left-0 right-0 outline-none rounded-t-6xl"
+        >
+          <VisuallyHidden.Root>
+            <Dialog.Title className="text-text-base font-medium">
+              Flight Details
+            </Dialog.Title>
+            <Dialog.Description className="text-text-subtle">
+              {flight.flightNumber}
+            </Dialog.Description>
+          </VisuallyHidden.Root>
           <div className="max-w-lg mx-auto px-4 pb-5">
             <div className="w-full flex justify-center py-2 mb-4">
               <div className="h-1 w-14 rounded-full bg-border-strong"></div>
@@ -44,23 +64,30 @@ const FlightDetails = ({ flight, isOpen, onOpenChange }: Props) => {
 
             <div className="flex gap-2 justify-between items-center border-t border-border-strong mt-5 pt-5 border-dashed">
               <div className="w-2/5">
-                <p className="text-text-subtle text-sm flex gap-1 items-center"><AirplaneTakeoff /> Departure</p>
-                <p className="text-text-base font-medium">{
-              flight.direction === "arrival" ? flight.destination : "Velana Int. Airport"
-              }</p>
+                <p className="text-text-subtle text-sm flex gap-1 items-center">
+                  <AirplaneTakeoff /> Departure
+                </p>
+                <p className="text-text-base font-medium">
+                  {flight.direction === "arrival"
+                    ? flight.destination
+                    : "Velana Int. Airport"}
+                </p>
                 <p className="text-text-subtle text-sm ">Feb 14, 10:32</p>
               </div>
-
 
               <div className="text-text-base rotate-90">
                 <Airplane className="w-6 h-6" fill="currentColor" />
               </div>
 
               <div className="w-2/5 text-right">
-                <p className="text-text-subtle text-sm flex gap-1 items-center justify-end"><AirplaneLanding /> Arrival</p>
-                <p className="text-text-base font-medium">{
-              flight.direction === "arrival" ? "Velana Int. Airport" : flight.destination
-              }</p>
+                <p className="text-text-subtle text-sm flex gap-1 items-center justify-end">
+                  <AirplaneLanding /> Arrival
+                </p>
+                <p className="text-text-base font-medium">
+                  {flight.direction === "arrival"
+                    ? "Velana Int. Airport"
+                    : flight.destination}
+                </p>
                 <p className="text-text-subtle text-sm ">Feb 14, 18:12</p>
               </div>
             </div>
@@ -78,10 +105,15 @@ const FlightDetails = ({ flight, isOpen, onOpenChange }: Props) => {
 
             <div className="flex gap-2 border-t border-border-strong mt-5 pt-5 border-dashed">
               <div className="flex gap-1">
-                <p className="text-text-subtle">{flight.direction === "arrival" ? "Baggage Claim" : "Gate"}: </p>
-                <p className="text-text-base font-medium">{flight.direction === "arrival" ? flight.baggageClaim : flight.gate}</p>
+                <p className="text-text-subtle">
+                  {flight.direction === "arrival" ? "Baggage Claim" : "Gate"}:{" "}
+                </p>
+                <p className="text-text-base font-medium">
+                  {flight.direction === "arrival"
+                    ? flight.baggageClaim
+                    : flight.gate}
+                </p>
               </div>
-
             </div>
           </div>
         </Drawer.Content>
